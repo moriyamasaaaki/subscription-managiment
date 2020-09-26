836aeb521b265ff3f2c56203a0ab101913edfe0c
moriyamasaaaki <moriya-7071@outlook.com>
2020-09-27 04:39:33 +0900

    feat(js,vue): vuetify導入

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
-<div id="app">
-    <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-</div>
+<v-app>
+    <v-app-bar app color="primary" dark>
+        <div class="d-flex align-center">
+            <h1>サブスク管理</h1>
+        </div>
+
+        <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+        <SubscriptionCard />
+    </v-main>
+</v-app>
 </template>
+
+<script>
+import SubscriptionCard from "@/components/SubscriptionCard";
+export default {
+    name: 'App',
+
+    components: {
+        SubscriptionCard
+    },
+
+    data: () => ({
+        //
+    }),
+};
+</script>
---
+++ src/components/SubscriptionCard.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="subscription-list">
+    <v-row no-gutters>
+        <v-col v-for="n in 100" :key="n" cols="12" sm="4">
+            <v-card class="subscription-list__card mx-auto" max-width="450">
+                <v-card-text>
+                    <div>カテゴリ</div>
+                    <p class="display-1 text--primary">サブスク名</p>
+                    <p>2020/09/27</p>
+                    <div class="text--primary">
+                        概要が入ります。概要が入ります。<br />
+                        概要が入ります。概要が入ります。概要が入ります。
+                    </div>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn text color="deep-purple accent-4"> Learn More </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-col>
+    </v-row>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.subscription-list {
+    margin-top: 40px;
+
+    &__card {
+        margin-bottom: 16px;
+    }
+}
+</style>
