dc28eef268ce500c89761913c9bff26833145073
moriyamasaaaki <moriya-7071@outlook.com>
2020-09-27 12:51:41 +0900

    feat(SubscriptionCard): from追加 #1

--- src/components/SubscriptionCard.vue
+++ src/components/SubscriptionCard.vue
@@ -12,14 +12,54 @@
                         概要が入ります。概要が入ります。概要が入ります。
                     </div>
                 </v-card-text>
-                <v-card-actions>
-                    <v-btn text color="deep-purple accent-4"> Learn More </v-btn>
-                </v-card-actions>
+                <v-form class="form" ref="form" v-model="valid" lazy-validation>
+
+                    <v-text-field v-model="description" :counter="100" :rules="descriptionRules" label="メモ" required></v-text-field>
+
+                    <v-select v-model="select" :items="items" :rules="[v => !!v || 'Item is required']" label="登録日" required></v-select>
+
+                    <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
+                        完了
+                    </v-btn>
+
+                    <v-btn color="error" class="mr-4" @click="reset">
+                        リセット
+                    </v-btn>
+                </v-form>
             </v-card>
         </v-col>
     </v-row>
 </div>
 </template>
+
+<script>
+export default {
+    data: () => ({
+        valid: true,
+        name: '',
+        nameRules: [
+            v => !!v || '必須項目です',
+            v => (v && v.length <= 100) || 'Name must be less than 100 characters',
+        ],
+        select: null,
+        items: [
+            '1',
+            '2',
+            '3',
+            '4',
+        ],
+    }),
+
+    methods: {
+        validate() {
+            this.$refs.form.validate()
+        },
+        reset() {
+            this.$refs.form.reset()
+        },
+    },
+}
+</script>
 
 <style lang="scss" scoped>
 .subscription-list {
@@ -28,5 +68,9 @@
     &__card {
         margin-bottom: 16px;
     }
+}
+
+.form {
+    padding: 16px;
 }
 </style>
