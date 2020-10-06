8fe4d90283007a1ebdfe4330fd4263c742d19a3a
moriyamasaaaki <moriya-7071@outlook.com>
2020-10-07 03:10:16 +0900

    style(Usage.vue): h1スタイル追加 #22

--- src/views/Usage.vue
+++ src/views/Usage.vue
@@ -1,5 +1,18 @@
 <template>
 <div class="usage">
-    <h1>使い方</h1>
+    <h1 class="hdg">使い方</h1>
 </div>
 </template>
+
+<style lang="scss" scoped>
+.hdg {
+    font-size: 20px;
+    padding: 8px 16px;
+    color: #fff;
+    background: #2c79ec;
+    -webkit-box-shadow: 5px 5px 0 rgb(2, 62, 112);
+    box-shadow: 5px 5px 0 rgb(2, 62, 112);
+    width: 170px;
+    margin: 40px auto;
+}
+</style>
