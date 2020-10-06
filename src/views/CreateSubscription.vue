eaf81b9078b7d4c461b12f1e403f24b0ff20206f
moriyamasaaaki <moriya-7071@outlook.com>
2020-10-06 12:31:05 +0900

    feat(CreateSubscription): form validationの実装 #16

--- src/views/CreateSubscription.vue
+++ src/views/CreateSubscription.vue
@@ -8,25 +8,34 @@
         <v-flex xs10 mt-5>
             <v-card>
                 <v-card-text>
-                    <v-form>
-                        <v-text-field type="text" v-model="subscription.name" label="サブスク名"></v-text-field>
-                        <v-text-field type="number" v-model.number="subscription.fee" label="料金"></v-text-field>
-                        <v-text-field type="text" v-model="subscription.description" label="概要"></v-text-field>
-                        <v-text-field type="text" v-model="subscription.memo" label="メモ"></v-text-field>
+                    <v-form ref="form">
+                        <v-text-field type="text" v-model="subscription.name" :rules="nameRules" :counter="20" required label="サブスク名"></v-text-field>
+                        <v-text-field type="number" v-model.number="subscription.fee" :rules="feeRules" required label="料金"></v-text-field>
+                        <v-text-field type="text" v-model="subscription.description" :counter="50" label="概要"></v-text-field>
+                        <v-text-field type="text" v-model="subscription.memo" :counter="100" label="メモ"></v-text-field>
                         <v-text-field type="text" v-model="subscription.url" label="サイトURL"></v-text-field>
-                        <v-select :items="months" v-model="subscription.month" label="登録月"></v-select>
-                        <v-select :items="days" v-model="subscription.day" label="登録日"></v-select>
+                        <v-select :items="months" v-model="subscription.month" :rules="[v => !!v || '*登録月は必須項目です。']" required label="登録月"></v-select>
+                        <v-select :items="days" v-model="subscription.day" :rules="[v => !!v || '*登録日は必須項目です。']" required label="登録日"></v-select>
 
                         <v-container fluid>
                             <p>プラン</p>
-                            <v-radio-group v-model="subscription.type" :mandatory="false">
+                            <v-radio-group v-model="subscription.type" :rules="[v => !!v || '*登録プランは必須項目です。']" required :mandatory="false">
                                 <v-radio label="月額" value="月額"></v-radio>
                                 <v-radio label="年額" value="年額"></v-radio>
                                 <v-radio label="お試し期間" value="お試し期間"></v-radio>
                             </v-radio-group>
                         </v-container>
                         <v-btn @click="$router.push({ name: 'subscriptions' })">キャンセル</v-btn>
-                        <v-btn color="info" @click="submit">作成</v-btn>
+                        <v-btn color="info" :disabled="
+                            !subscription.name ||
+                            !subscription.fee ||
+                            !subscription.month ||
+                            !subscription.day ||
+                            !subscription.type ||
+                            subscription.name.length > 20 ||
+                            subscription.description.length > 50 ||
+                            subscription.memo.length > 100
+                        " @click="submit">作成</v-btn>
                     </v-form>
                 </v-card-text>
             </v-card>
@@ -44,6 +53,13 @@ export default {
         return {
             months: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
             days: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+            nameRules: [
+                v => !!v || '*サブスク名は必須項目です。',
+                v => (v && v.length <= 20) || '*サブスク名は20文字以内にしてください。',
+            ],
+            feeRules: [
+                v => !!v || '*料金は必須項目です。',
+            ],
             subscription: {}
         }
     },
